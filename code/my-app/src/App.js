21d417a762b8dae8fbde88140de0ebe095353e2c
Monisha Sugirtharajan <e22381@eng.pdn.ac.lk>
2026-02-10 11:20:45 +0530

    Add initial components and update App.js

--- code/my-app/src/App.js
+++ code/my-app/src/App.js
@@ -1,9 +1,12 @@
 import Welcome from "./components/Welcome";
+import CustomerDashboard from "./components/CustomerDashboard";
+
 
 function App() {
   return (
     <div>
       <Welcome />
+      <CustomerDashboard />
     </div>
   );
 }
