a68d8b5e9cc2f65b340c760180af70c4c783fcff
s-monisha11 <e22381@eng.pdn.ac.lk>
2026-03-08 19:44:16 +0530

    save my local changes

---
+++ code/frontend/src/utils/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: "nagromsnew.firebaseapp.com",
+    projectId: "nagromsnew",
+    storageBucket: "nagromsnew.firebasestorage.app",
+    messagingSenderId: "28463182267",
+    appId: "1:28463182267:web:b76a1f04988a35f3ce149e"
+}
